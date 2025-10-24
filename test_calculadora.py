5419efbc4876186e1811a0535eb2d9f32dc34962
DanyArandaCazares <A346027@alumnos.uaslp.mx>
2025-10-23 20:18:40 -0600

    prueba

---
+++ test_calculadora.py
@@ -0,0 +1,24 @@
+import pytest
+from calculadora import sumar, restar, multiplicar, dividir
+
+def test_sumar_positivos():
+    assert sumar(5, 5) == 10
+
+def test_sumar_negativos():
+    assert sumar(-1, -1) == -2
+
+def test_sumar_positivo_y_negativo():
+    assert sumar(10, -5) == 5
+
+def test_restar():
+    assert restar(10, 5) == 5
+
+def test_multiplicar():
+    assert multiplicar(3, 3) == 9
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+
+def test_dividir_por_cero():
+    with pytest.raises(ValueError):
+        dividir(10, 0)
